019e38a250a7da4967768aa6de4f3eb08533364d
student <lyceum16bala@gmail.com>
2026-07-09 12:43:30 +0300

    Фінилізовано мікроблог

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path(r'', views.post_list, name='post_list'),
     path(r'post/<int:post_id>/', views.post_detail, name='post_detail'),
     path(r'post/new/', views.post_new, name='post_new'),
+    path(r'post/edit/<int:post_id>/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from blog.forms import PostForm
 from blog.models import Post
 from django.utils import timezone
-
+from django.conf import settings
 
 # Create your views here.
 def post_list(request):
@@ -12,6 +13,8 @@ def post_list(request):
     return render(request, 'post_list.html', {'posts': posts})
 
 def post_new(request):
+    if not request.user.is_authenticated:
+        return redirect("/admin/login/")
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
@@ -20,6 +23,8 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post_id=post.id)
+        else:
+            messages.error(request, form.errors)
     form = PostForm()
     return render(request, 'post_new.html', {'form': form})
 
@@ -27,3 +32,20 @@ def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'post_detail.html', {'post': post})
 
+def post_edit(request, post_id):
+    if not request.user.is_authenticated:
+        return redirect("/admin/login/")
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            print(request.user)
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post_id=post.id)
+        else:
+            messages.error(request, form.errors)
+    form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form': form})
